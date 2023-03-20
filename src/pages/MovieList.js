f43a2ec3f0dbad1f5626d4b1023cba24c97eeb1f
Aleksandar Markovic <alekbg99@gmail.com>
2023-03-20 07:42:52 +0100

    Kreiranje custom hook-a i sadrzaja ostalih strana

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 import { Card } from "../components";
 
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=26b6868804601d44d5ec1fb3c1ba2126"
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
